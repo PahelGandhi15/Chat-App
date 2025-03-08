2b9470c2a4edb500f401e45b97ee5f497ce61f4a
Pahel Gandhi <gandhipahel@gmail.com>
2025-03-08 21:47:12 +0530

    latest

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,9 +1,56 @@
-const ContactsContainer= () => {
-    return(
+const ContactsContainer = () => {
+    return (
         <div className="relative md:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
-            ContactsContainer
+            <div className="pt-3">
+                <Logo />
+                <div className="my-5">
+                    <div className="flex items-center justify-between pr-10">
+                        <Title text="Direct Messages" />
+                    </div>
+                </div>
+                <div className="my-5">
+                    <div className="flex items-center justify-between pr-10">
+                        <Title text="Channels" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default ContactsContainer;
+
+const Logo = () => {
+    return (
+        <div className="flex p-5 justify-start items-center gap-2">
+            <svg
+                id="logo-38"
+                width="78"
+                height="32"
+                viewBox="0 0 78 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M35.5 0H31.5L12.5 32H16.5L35.5 0"
+                    className="ccustom"
+                    fill="#8338ec"
+                />
+                <path
+                    d="M19.5 0H31.5L12.5 32H0.5L19.5 0"
+                    className="ccompli2"
+                    fill="#a16ee8"
+                />
+            </svg>
+            <span className="text-3xl font-semibold">Syncronus</span>
+        </div>
+    );
+};
+
+const Title = ({ text }) => {
+    return (
+        <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">
+            {text}
+        </h6>
+    );
+};
